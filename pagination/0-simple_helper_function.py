3910b28bb35e3f89f2db8d0075e115fb0c3d8068
bayramovh062-ui <bayramovh062@gmail.com>
2026-07-15 21:49:28 +0400

    add main.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-that is a module that it remains itself function
+example
 """
 from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    this function returns a tuple corresponding to the range of indexes
+    example
     """
 
     return ((page - 1) * page_size, page * page_size)
