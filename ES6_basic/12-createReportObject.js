cd95e7636f250e212cdb0934b2bfad3d4700713e
bayramovh062-ui <bayramovh062@gmail.com>
2026-07-24 19:16:52 +0400

    Create 12-createReportObject.js

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {...employeesList},
+    getNumberOfDepartments(employeeList){
+      return Object.keys(employeeList).length
+    }
+  } 
+}
