d4ce361f956dfb0aa9a19a9bac03ae8fef1ea482
bayramovh062-ui <bayramovh062@gmail.com>
2026-07-25 21:13:20 +0400

    create 6-sky_highs.js

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+import Building from "./5-building.js";
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    this._sqft = sqft
+    this._floors = floors
+  }
+
+  get sqft() {
+    return this._sqft
+  }
+
+  get floors() {
+    return this._floors
+  }
+
+  evacuationWarningMessage() {
+    return "Evacuate slowly the NUMBER_OF_FLOORS floors"
+  }
+}
