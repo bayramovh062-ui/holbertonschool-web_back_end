9592e4596444490dec39b9b4b80e64c20056174c
bayramovh062-ui <bayramovh062@gmail.com>
2026-07-25 21:16:50 +0400

    fix inheritance problems

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,20 +1,16 @@
-import Building from "./5-building.js";
+import Building from './5-building.js';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    this._sqft = sqft
-    this._floors = floors
-  }
-
-  get sqft() {
-    return this._sqft
+    super(sqft);
+    this._floors = floors;
   }
 
   get floors() {
-    return this._floors
+    return this._floors;
   }
 
   evacuationWarningMessage() {
-    throw new Error("Evacuate slowly the NUMBER_OF_FLOORS floors")
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
