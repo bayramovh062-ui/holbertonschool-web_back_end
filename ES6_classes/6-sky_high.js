3524f0e7ddaf92fac152e3df87d04a360de016eb
bayramovh062-ui <bayramovh062@gmail.com>
2026-07-25 21:14:20 +0400

    create 6-sky_highs.js

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -15,6 +15,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return "Evacuate slowly the NUMBER_OF_FLOORS floors"
+    throw new Error("Evacuate slowly the NUMBER_OF_FLOORS floors")
   }
 }
