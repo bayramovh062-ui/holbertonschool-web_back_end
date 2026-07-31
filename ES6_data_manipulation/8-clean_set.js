e251e2412e42d3a263a0aa546da5ebd81c92ae06
bayramovh062-ui <bayramovh062@gmail.com>
2026-07-31 22:46:51 +0400

    fix return result

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
 export default function cleanSet(set, startString) {
-    let result = ''
+  let result = ''
   for (let value of set){
     if(value.startsWith(startString)){
         result += value.splice(0, startString.length) + '-'
     }
   }
+  return result
 }
