2632af84c09902e8ad0aab259c92fa104c2768ff
bayramovh062-ui <bayramovh062@gmail.com>
2026-07-31 23:04:38 +0400

    fix

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,14 @@
 export default function cleanSet(set, startString) {
-  if(!startString || typeof startString !== 'string'){
-    return ''
+  if (!startString || typeof startString !== 'string') {
+    return '';
   }
-  let result = ''
-  for (let value of set){
-    if(value.startsWith(startString)){
-      result += value.slice(startString.length, value.length) + '-'
+  
+  let result = '';
+  for (let value of set) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      result += value.slice(startString.length, value.length) + '-';
     }
   }
-  return result.slice(0, result.length -1)
+  
+  return result.slice(0, result.length - 1);
 }
