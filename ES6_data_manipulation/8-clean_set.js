9a05a539b329a6400bd9291b734f0f3fafb431ee
bayramovh062-ui <bayramovh062@gmail.com>
2026-07-31 23:00:48 +0400

    replace splice to slice

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
-    if(startString === ''){
-        return ''
-    }
+  if(!startString){
+    return ''
+  }
   let result = ''
   for (let value of set){
     if(value.startsWith(startString)){
