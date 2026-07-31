f1fa357dde4662d82ada0d421975defac6d74859
bayramovh062-ui <bayramovh062@gmail.com>
2026-07-31 22:58:54 +0400

    replace splice to slice

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
 export default function cleanSet(set, startString) {
+    if(startString === ''){
+        return ''
+    }
   let result = ''
   for (let value of set){
     if(value.startsWith(startString)){
-      result += value.slice(startString.length -1, value.length) + '-'
+      result += value.slice(startString.length, value.length) + '-'
     }
-    return result.slice(0, result.length -1)
   }
+  return result.slice(0, result.length -1)
 }
