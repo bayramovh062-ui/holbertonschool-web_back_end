31ced4c4621142a254c844bb86a8c34d3b12f5bc
bayramovh062-ui <bayramovh062@gmail.com>
2026-08-02 20:27:44 +0400

    add 1-promise.js

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+export const getFullResponseFromAPI = (flag) => {
+  return new Promise((resolve, reject) => {
+    if(flag) {
+      resolve({status: 200, body: "Succes"})
+    }else {
+      reject("The fake API is not working currently")
+    }
+  })
+}
