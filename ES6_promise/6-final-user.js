5c7ed6f643e179706ce94f652da704f12d4efcad
bayramovh062-ui <bayramovh062@gmail.com>
2026-08-02 22:26:46 +0400

    fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,15 +1,14 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return [
-    {
-        status: signUpUser(firstName, lastName).status,
-        value: signUpUser(firstName, lastName).value
-    },
-    {
-        status: uploadPhoto(fileName).status,
-        value: uploadPhoto(fileName).value
-    }
-  ]
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }))
+  );
 }
